f1aa11e918b1cd2bcb82485453ac965f31ab59a0
GerasimosKan <46671986+GerasimosKan@users.noreply.github.com>
2019-06-19 20:21:14 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const {Client, Attachment} = require("discord.js");
+const bot = new Client();
+
+//TOKEN GIA BOT
+const token ="NTkwOTE4OTAzNzc4MjQ2NjU2.XQpnvA.XP-N5QBujlHi1tI_hqdTRNQoJJg";
+bot.login(token);
+
+//Start command
+const PREFIX = "!";
+
+//Bot online
+bot.on("ready", ()=> {
+    console.log("BOT ONLINE");
+});
+
+//Wellcome message
+bot.on('guildMemberAdd', member =>{
+    const channel = member.guild.channels.find(channel => channel.name === "general");
+    if (!channel) return;
+
+    channel.send(`Όπα καλωσόρισες ΨΑΡΑΚΛΑ ${member} πάνε καθάρισε τις καλλιόπες`)
+});
+
+
+//OPA FILE
+bot.on("message", message=>{
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'send':
+                const filos = new Attachment('https://i.imgur.com/32n7Fi8.png')
+                message.channel.send(message.author, filos);
+            break;
+    }
+})
