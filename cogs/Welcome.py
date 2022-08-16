6f3cf44af9994b8478f820f5be8491b0b0476dfa
Gerasimos Kanellatos <46671986+GerasimosKan@users.noreply.github.com>
2022-08-16 18:19:57 +0300

    Update Welcome.py

--- cogs/Welcome.py
+++ cogs/Welcome.py
@@ -1,17 +1,13 @@
-#Role = 546665144822923265
-#Channel = 546662624243220494
-
+from GlobalVar import Role, Channel
 import discord
 import asyncio
-
 from PIL import Image
 from io import BytesIO
 from turtle import stamp
 from PIL import ImageFont
 from PIL import ImageDraw
-from discord.utils import get
+from discord.utils import *
 from discord.ext import commands
-from GlobalVar import Role, Channel
 from discord.ext.commands import Bot
 
 
@@ -20,27 +16,26 @@ class Welcome(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_member_join(self, ctx):
+    async def on_member_join(self, member: discord.Member):
+
         guild = member.guild
         channel = self.bot.get_channel(Channel)
-        role = get(guild.roles, id=Role)
+        role = discord.utils.get(member.guild.roles, id=Role)
 
-        img = Image.open('Discord_Bot/images/id.jpg') #image
-        
-        author = ctx.message.author
-        pfpi = author.avatar_url_as
-
-        asset = pfpi(size=512)
+        img = Image.open('./images/id.jpg')  # image
+        asset = member.avatar_url_as(size=512)
         data = BytesIO(await asset.read())
         pfp = Image.open(data)
 
-        pfp = pfp.resize((178,180)) #border size
-        img.paste(pfp, (119,63)) #image paste location
+        pfp = pfp.resize((178, 180))  # border size
+        img.paste(pfp, (119, 63))  # image paste location
 
         img.save("MyID.jpg")
+        # End of image processing
 
-        await channel.send(f"Welcome, {member.mention}, Η καινούργια σου ταυτότητα!\n", file = discord.File("MyID.jpg"))    
+        await channel.send(f"Welcome, {member.mention}, Η καινούργια σου ταυτότητα!\n", file=discord.File("MyID.jpg"))
         await member.add_roles(role)
+
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
